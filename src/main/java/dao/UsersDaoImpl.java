a667a2ee987a5739199f62202a82d7197cf868bb
Dmitry Yukhlin <deemansm@gmail.com>
2017-11-16 22:22:21 +0400

    Refactoring of Hibernate method: deleted catch blocks for unchecked HibernateException; try-catch replaced by try-with-resources

--- src/main/java/dao/UsersDaoImpl.java
+++ src/main/java/dao/UsersDaoImpl.java
@@ -1,7 +1,6 @@
 package dao;
 
 import model.User;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
@@ -10,19 +9,13 @@ import java.util.List;
 
 @Component("UsersDao")
 public class UsersDaoImpl implements UsersDao {
-    private Session session;
 
     @Override
     public List<User> getAll() {
         List<User> users = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<User> query = session.createQuery("FROM User", User.class);
             users = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
         return users;
     }
@@ -30,15 +23,10 @@ public class UsersDaoImpl implements UsersDao {
     @Override
     public List<User> getByName(String name) {
         List<User> users = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<User> query = session.createQuery("FROM User WHERE name=:paramName", User.class);
             query.setParameter("paramName", name);
             users = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
         return users;
     }
@@ -46,75 +34,51 @@ public class UsersDaoImpl implements UsersDao {
     @Override
     public User getById(int id) {
         User user = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            Query query = session.createQuery("FROM User WHERE id=:paramId");
+        Query query;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            query = session.createQuery("FROM User WHERE id=:paramId");
             query.setParameter("paramId", id);
             user = (User) query.uniqueResult();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
         return user;
     }
 
     @Override
     public void saveUser(User user) {
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
     }
 
     @Override
     public void updateUser(User user) {
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.update(user);
             session.getTransaction().commit();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
     }
 
     @Override
     public void deleteUser(int id) {
-        try {
-            User user = getById(id);
-            session = HibernateUtil.getSessionFactory().openSession();
+        User user = getById(id);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             session.beginTransaction();
             session.delete(user);
             session.getTransaction().commit();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
     }
 
     @Override
     public List<User> getUsersPerPage(int fromRow, int numberOfRows) {
         List<User> users = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<User> query = session.createQuery("FROM User", User.class);
             query.setFirstResult(fromRow);
             query.setMaxResults(numberOfRows);
             users = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
         }
         return users;
     }
